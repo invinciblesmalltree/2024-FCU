1ed359600df5ae3d9f2f40cf5f27b50afab35d47
invinciblesmalltree <2581283397@qq.com>
2024-07-30 22:22:13 +0800

    fix some bugs

--- src/screen/scripts/screen.py
+++ src/screen/scripts/screen.py
@@ -5,103 +5,31 @@ from std_msgs.msg import Int32
 from screen.msg import goods_info
 import json
 
-json_data={
-    "A3": {
-        "value": -1,
-        "coordinate": [0.0, 0.75, 1.25, 0]
-    },
-    "A2": {
-        "value": -1,
-        "coordinate": [0.0, 1.25, 1.25, 0]
-    },
-    "A1": {
-        "value": -1,
-        "coordinate": [0.0, 1.75, 1.25, 0]
-    },
-    "A4": {
-        "value": -1,
-        "coordinate": [0.0, 1.75, 0.85, 0]
-    },
-    "A5": {
-        "value": -1,
-        "coordinate": [0.0, 1.25, 0.85, 0]
-    },
-    "A6": {
-        "value": -1,
-        "coordinate": [0.0, 0.75, 0.85, 0]
-    },
-    "C6": {
-        "value": -1,
-        "coordinate": [1.75, 0.75, 0.85, 0]
-    },
-    "C5": {
-        "value": -1,
-        "coordinate": [1.75, 1.25, 0.85, 0]
-    },
-    "C4": {
-        "value": -1,
-        "coordinate": [1.75, 1.75, 0.85, 0]
-    },
-    "C1": {
-        "value": -1,
-        "coordinate": [1.75, 1.75, 1.25, 0]
-    },
-    "C2": {
-        "value": -1,
-        "coordinate": [1.75, 1.25, 1.25, 0]
-    },
-    "C3": {
-        "value": -1,
-        "coordinate": [1.75, 0.75, 1.25, 0]
-    },
-    "B1": {
-        "value": -1,
-        "coordinate": [1.75, 0.75, 1.25, 3.14159]
-    },
-    "B2": {
-        "value": -1,
-        "coordinate": [1.75, 1.25, 1.25, 3.14159]
-    },
-    "B3": {
-        "value": -1,
-        "coordinate": [1.75, 1.75, 1.25, 3.14159]
-    },
-    "B4": {
-        "value": -1,
-        "coordinate": [1.75, 0.75, 0.85, 3.14159]
-    },
-    "B5": {
-        "value": -1,
-        "coordinate": [1.75, 1.25, 0.85, 3.14159]
-    },
-    "B6": {
-        "value": -1,
-        "coordinate": [1.75, 0.75, 0.85, 3.14159]
-    },
-    "D4": {
-        "value": -1,
-        "coordinate": [3.50, 0.75, 0.85, 3.14159]
-    },
-    "D5": {
-        "value": -1,
-        "coordinate": [3.50, 1.25, 0.85, 3.14159]
-    },
-    "D6": {
-        "value": -1,
-        "coordinate": [3.50, 1.75, 0.85, 3.14159]
-    },
-    "D1": {
-        "value": -1,
-        "coordinate": [3.50, 1.75, 1.25, 3.14159]
-    },
-    "D2": {
-        "value": -1,
-        "coordinate": [3.50, 1.25, 1.25, 3.14159]
-    },
-    "D3": {
-        "value": -1,
-        "coordinate": [3.50, 0.75, 1.25, 3.14159]
-    }
+json_data = {
+    "A3": {"value": -1, "coordinate": [0.0, 0.75, 1.25, 0]},
+    "A2": {"value": -1, "coordinate": [0.0, 1.25, 1.25, 0]},
+    "A1": {"value": -1, "coordinate": [0.0, 1.75, 1.25, 0]},
+    "A4": {"value": -1, "coordinate": [0.0, 1.75, 0.85, 0]},
+    "A5": {"value": -1, "coordinate": [0.0, 1.25, 0.85, 0]},
+    "A6": {"value": -1, "coordinate": [0.0, 0.75, 0.85, 0]},
+    "C6": {"value": -1, "coordinate": [1.75, 0.75, 0.85, 0]},
+    "C5": {"value": -1, "coordinate": [1.75, 1.25, 0.85, 0]},
+    "C4": {"value": -1, "coordinate": [1.75, 1.75, 0.85, 0]},
+    "C1": {"value": -1, "coordinate": [1.75, 1.75, 1.25, 0]},
+    "C2": {"value": -1, "coordinate": [1.75, 1.25, 1.25, 0]},
+    "C3": {"value": -1, "coordinate": [1.75, 0.75, 1.25, 0]},
+    "B1": {"value": -1, "coordinate": [1.75, 0.75, 1.25, 3.14159]},
+    "B2": {"value": -1, "coordinate": [1.75, 1.25, 1.25, 3.14159]},
+    "B3": {"value": -1, "coordinate": [1.75, 1.75, 1.25, 3.14159]},
+    "B4": {"value": -1, "coordinate": [1.75, 0.75, 0.85, 3.14159]},
+    "B5": {"value": -1, "coordinate": [1.75, 1.25, 0.85, 3.14159]},
+    "B6": {"value": -1, "coordinate": [1.75, 0.75, 0.85, 3.14159]},
+    "D4": {"value": -1, "coordinate": [3.50, 0.75, 0.85, 3.14159]},
+    "D5": {"value": -1, "coordinate": [3.50, 1.25, 0.85, 3.14159]},
+    "D6": {"value": -1, "coordinate": [3.50, 1.75, 0.85, 3.14159]},
+    "D1": {"value": -1, "coordinate": [3.50, 1.75, 1.25, 3.14159]},
+    "D2": {"value": -1, "coordinate": [3.50, 1.25, 1.25, 3.14159]},
+    "D3": {"value": -1, "coordinate": [3.50, 0.75, 1.25, 3.14159]},
 }
 
 
@@ -138,19 +66,21 @@ def get_address_by_value(value):
 def search_all():
     i = 0
     for address, value in json_data.items():
-        ser.write(f"page4.n{i}.val={value}\xff\xff\xff".encode())
+        ser.write(f"page4.n{i}.val={value}" + b"\xff\xff\xff".encode())
         i = i + 1
 
 
 # 货物编号为1～24的数值；坐标信息为A1~A6、B1~B6、C1~C6、D1~D6
 def goods_callback(msg):
     update_json_value(msg.address, msg.value)
-    ser.write(b"page1.t4.txt=%d\xff\xff\xff", msg.value)
-    ser.write(b"page1.t6.txt=%d\xff\xff\xff", msg.address)  # 实时发送编号及坐标至串口屏
+    ser.write(f"page1.t4.txt={msg.value}" + b"\xff\xff\xff")
+    ser.write(
+        f"page1.t6.txt={msg.address}" + b"\xff\xff\xff"
+    )  # 实时发送编号及坐标至串口屏
 
 
 # 配置串口
-ser = serial.Serial("/dev/AMA0", baudrate=9600, timeout=1)
+ser = serial.Serial("/dev/ttyS6", baudrate=9600, timeout=1)
 # 起飞发布，发1为盘点程序，发2为定向程序
 pub = rospy.Publisher("/offboard_order", Int32, queue_size=10)
 # 从scanner订阅货物信息
@@ -173,7 +103,7 @@ while not rospy.is_shutdown():
             elif line.startwith("search"):  # 评委查询编号
                 num = int(line[2:])
                 ser.write(
-                    b'page3.t4.txt="%s"\xff\xff\xff', get_address_by_value(num)
+                    f'page3.t4.txt="{get_address_by_value(num)}"' + b"\xff\xff\xff"
                 )  # 评委查询编号后发送坐标至串口屏
             elif line == "search_all":  # 串口屏显示所有货物信息
                 search_all()
